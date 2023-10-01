55ffb57065c5c0736dfec5d648ec4d5962e5e9cf
DannyRand46 <williamsrandon46@gmail.com>
2023-10-01 00:17:13 -0500

    Made basic weapons for enemies.
    Again needs some testing (more so for the melee enemy).

---
+++ Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+    [Header("----- Components -----")]
+    [SerializeField] Rigidbody rb;
+
+    [Header("----- Stats -----")]
+    [SerializeField] int damage;
+    [SerializeField] int lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+        {
+            return;
+        }
+
+        IDamage damageable = other.GetComponent<IDamage>();
+        if (damageable != null)
+        {
+            damageable.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Header("----- Components -----")]
+    [SerializeField] Rigidbody rb;
+
+    [Header("----- Components -----")]
+    [SerializeField] int damage;
+    [SerializeField] float speed;
+    [SerializeField] int lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.isTrigger)
+        {
+            return;
+        }
+
+        IDamage damageable = other.GetComponent<IDamage>();
+        if(damageable != null )
+        {
+            damageable.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}

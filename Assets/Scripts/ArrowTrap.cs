5092160cc0adf8d934e6ffdf2884a070bf3d4053
Kevin Hernandez <KJHernandez1@student.fullsail.edu>
2023-10-04 21:09:38 -0400

    arrow trap and arrow prefabs
    working on fixing arrow movement and arrowtrap

---
+++ Assets/Scripts/ArrowTrap.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+    private GameObject arrowPrefab;
+    private Transform player;
+    float arrowSpeed = 2;
+
+    private bool isPressed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !isPressed)
+        {
+            isPressed = true;
+            SpawnArrow();
+        }
+    }
+
+    private void SpawnArrow()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        GameObject arrow = Instantiate(arrowPrefab, transform.position + Vector3.up, Quaternion.identity);
+        ArrowMovement arrowMovement = arrow.GetComponent<ArrowMovement>();
+    }
+}

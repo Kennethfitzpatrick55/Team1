956567eb5c7be6d468854bcac134c2d0d0e8f7b0
Mikeybuzz <114454673+Mikeybuzz@users.noreply.github.com>
2023-10-16 08:41:42 -0400

    Spawner work

--- Assets/Scripts/ArrowTrap.cs
+++ Assets/Scripts/ArrowTrap.cs
@@ -4,29 +4,28 @@ using UnityEngine;
 
 public class ArrowTrap : MonoBehaviour
 {
-    [Header("----- Components -----")]
     private GameObject arrowPrefab;
     private Transform player;
     float arrowSpeed = 2;
 
-    [Header("----- Bools -----")]
     private bool isPressed = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        // When the player walks over a tagged spot shoot the arrow
         if(other.CompareTag("Player") && !isPressed)
         {
-            isPressed = true;
+            Debug.Log("Incoming!");
             SpawnArrow();
         }
     }
 
     private void SpawnArrow()
     {
-        //spawns the arrow 
         Vector3 direction = (player.position - transform.position).normalized;
         GameObject arrow = Instantiate(arrowPrefab, transform.position + Vector3.up, Quaternion.identity);
-        ArrowMovement arrowMovement = arrow.GetComponent<ArrowMovement>();
+        ArrowMovement arrowMovement = arrow.GetComponent<arrowMovement>();
     }
+
+    
+
 }
---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newBehaviorScript : MonoBehaviour
+{
+    [SerializeField] List<GameObject> objectList = new List<GameObject>();
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] int maxObjectsToSpawn;
+    [SerializeField] int timeBetweenSpawn;
+    [SerializeField] Transform[] spawnPos;
+
+    int curObjectsSpawned;
+    bool isSpawning;
+    bool startSpawning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.updateGameGoal(maxObjectsToSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startSpawning && curObjectsSpawned < maxObjectsToSpawn)
+        {
+            StartCoroutine(spawn());
+        }
+    }
+
+    IEnumerator spawn()
+    {
+        if (!isSpawning)
+        {
+            isSpawning = true;
+
+            curObjectsSpawned++;
+            GameObject objectClone = Instantiate(objectToSpawn, spawnPos[Random.Range(0, spawnPos.Length)].position, transform.rotation);
+            objectList.Add(objectClone);
+            yield return new WaitForSeconds(timeBetweenSpawn);
+            isSpawning = false;
+        }
+    }
+
+    public void updateObjectNum()
+    {
+        curObjectsSpawned--;
+        maxObjectsToSpawn--;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = false;
+
+            for (int i = 0; i < objectList.Count; i++)
+            {
+                Destroy(objectList[i]);
+            }
+            objectList.Clear();
+
+            curObjectsSpawned = 0;
+        }
+    }
+}

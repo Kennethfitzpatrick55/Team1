748c806ba736856323f227cbf9c06fa029d4030d
DannyRand46 <williamsrandon46@gmail.com>
2023-10-18 15:04:05 -0500

    Cleanup and Updates
    - Removed obsolete melee enemy assets
    - Ranged enemy animated and functional script wise

--- Assets/Scripts/AIEnemyRanged.cs
+++ Assets/Scripts/AIEnemyRanged.cs
@@ -9,11 +9,15 @@ public class AIEnemyRanged : MonoBehaviour, IDamage
     [Header("----- Components -----")]
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Renderer model;
+    [SerializeField] Animator anim;
+    [SerializeField] Collider hitBox;
 
     [Header("----- Stats -----")]
     [Range(1, 25)][SerializeField] int HP;
     [SerializeField] int turnSpeed;
     [Range(1, 180)][SerializeField] int viewAngle;
+    [SerializeField] int roamDist;
+    [SerializeField] int roamTime;
 
     [Header("----- Attack Stats -----")]
     //Weapon implementation
@@ -26,16 +30,15 @@ public class AIEnemyRanged : MonoBehaviour, IDamage
     bool isAttacking;
     bool playerInRange;
     float angleToPlayer;
-    float deathAnimTime;
+    float stoppingDistOrig;
+    bool destinationChosen;
     Vector3 playerDir;
     Color colorOrig;
+    Vector3 startingPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        //FOR FUTURE USE, sets timer to destroy an enemy to allow for death animations to play (defaulting 0 for now)
-        deathAnimTime = 0;
-
         //Fetches original color to not mess up color/model after damage flash
         colorOrig = model.material.color;
 
@@ -44,18 +47,30 @@ public class AIEnemyRanged : MonoBehaviour, IDamage
 
         //Update enemy count with each spawn
         GameManager.instance.UpdateEnemyCount(1);
+
+        //Save starting position for roaming
+        startingPos = transform.position;
+
+        //Save original stopping distance for manipulation
+        stoppingDistOrig = agent.stoppingDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Resets enemy tracking if the player has respawned (fixes respawn not triggering exit)
-        RespawnReset();
-
-        //Tracks to the player if they are in range
-        if (playerInRange && CanSeePlayer())
+        if (agent.isActiveAndEnabled)
         {
+            anim.SetFloat("Speed", agent.velocity.normalized.magnitude);
 
+            //Tracks to the player if they are in range
+            if (!playerInRange)
+            {
+                StartCoroutine(Roam());
+            }
+            else if (playerInRange && !CanSeePlayer())
+            {
+                StartCoroutine(Roam());
+            }
         }
     }
 
@@ -69,11 +84,15 @@ public class AIEnemyRanged : MonoBehaviour, IDamage
 
         //Raycast to see if enemy can see player
         RaycastHit hit;
+        Debug.DrawRay(transform.position, playerDir);
         if (Physics.Raycast(transform.position, playerDir, out hit))
         {
             //See if enemy can "see" player
             if (hit.collider.CompareTag("Player") && angleToPlayer <= viewAngle)
             {
+                //Set to regular stopping distance for player tracking
+                agent.stoppingDistance = stoppingDistOrig;
+
                 //Turns towards player when not moving
                 if (agent.remainingDistance < agent.stoppingDistance)
                 {
@@ -91,28 +110,56 @@ public class AIEnemyRanged : MonoBehaviour, IDamage
                 //Return true if player is found properly
                 output = true;
             }
+            else
+            {
+                //Make stopping distance 0 for roaming
+                agent.stoppingDistance = 0;
+            }
         }
         return output;
+    }
+
+    IEnumerator Roam()
+    {
+        if (agent.remainingDistance < 0.05f && !destinationChosen)
+        {
+            destinationChosen = true;
+            agent.stoppingDistance = 0;
+
+            yield return new WaitForSeconds(roamTime);
+
+            Vector3 randomPos = UnityEngine.Random.insideUnitSphere * roamDist;
+            randomPos += startingPos;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomPos, out hit, roamDist, 1);
+            agent.SetDestination(hit.position);
+
+            destinationChosen = false;
+        }
     }
 
     public void TakeDamage(int amount)
     {
         HP -= amount;
-
-        //Have enemy respond to taking damage from player
-        agent.SetDestination(GameManager.instance.player.transform.position);
-        FaceTarget();
-
-        //Visual queue for damage taken
-        StartCoroutine(FlashDamage());
-
         if (HP <= 0)
         {
             //Remove enemy from count on death
             GameManager.instance.UpdateEnemyCount(-1);
+            anim.SetBool("Dead", true);
+            agent.enabled = false;
+            hitBox.enabled = false;
+            StopAllCoroutines();
+        }
+        else
+        {
+            //Have enemy respond to taking damage from player
+            agent.SetDestination(GameManager.instance.player.transform.position);
+            FaceTarget();
 
-            //Set time for future death animations
-            Destroy(gameObject, deathAnimTime);
+            //Visual queue for damage taken
+            StartCoroutine(FlashDamage());
+            anim.SetTrigger("Damage");
         }
     }
 
@@ -129,12 +176,16 @@ public class AIEnemyRanged : MonoBehaviour, IDamage
     {
         isAttacking = true;
 
-        //Creates weapon to use
-        Instantiate(weapon, weaponPos.position, transform.rotation);
+        anim.SetTrigger("Attack");
 
         yield return new WaitForSeconds(attackDelay);
 
         isAttacking = false;
+    }
+
+    public void CreateAttack()
+    {
+        Instantiate(weapon, weaponPos.position, transform.rotation);
     }
 
     //Provides rotation funcionality to track the player

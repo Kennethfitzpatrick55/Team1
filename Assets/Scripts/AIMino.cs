80121c9a342a7b9519a781ef7e3126fd8631d2f3
Tyler Linton <TSLinton@student.fullsail.edu>
2023-11-02 23:58:57 -0500

    Minitaur First round Changes Finalized
    Significant bugs arrived in this stage and delayed progess.

--- Assets/Scripts/AIMino.cs
+++ Assets/Scripts/AIMino.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class AIMino : MonoBehaviour, IDamage
 {
+    enum bossStages
+    {
+        FIRST_STAGE,
+        SECOND_STAGE,
+        THIRD_STAGE,
+        numberOfStages,
+    }
+
     [Header("----- Components -----")]
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Animator anim;
@@ -20,22 +30,51 @@ public class AIMino : MonoBehaviour, IDamage
     [SerializeField] int attackDelay;
     [SerializeField] float range;
 
+    float currSpeed;
+    float scalar;
+    float stageTwoSpeed;
+    bool isGrowing;
     float angleToPlayer;
     Vector3 playerDir;
+    bossStages currentStage;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentStage = bossStages.FIRST_STAGE;
+        isGrowing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //debug and testing keys for minatour stages
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            StartStageTwo();
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            StartStageThree();
+            
+        }
         if (agent.isActiveAndEnabled)
         {
             anim.SetFloat("Speed", agent.velocity.normalized.magnitude);
             Movement();
+        }
+        if (isGrowing)
+        {
+            scalar += Time.deltaTime;
+            agent.speed = 0;
+            transform.localScale = new Vector3(Mathf.Lerp(1.5f, 3f, ParametricGrowthCurve(scalar)), Mathf.Lerp(1.5f, 3f, ParametricGrowthCurve(scalar)), Mathf.Lerp(1.5f, 3f, ParametricGrowthCurve(scalar)));
+            if (scalar >= 2)
+            {
+                isGrowing = false;
+                agent.speed = stageTwoSpeed;
+            }
         }
     }
 
@@ -60,7 +99,7 @@ public class AIMino : MonoBehaviour, IDamage
         }
         else
         {
-            anim.SetBool("In Attack Range", false);
+            anim.SetInteger("In Attack Range", 0);
         }
     }
 
@@ -100,11 +139,13 @@ public class AIMino : MonoBehaviour, IDamage
         //If player is in range, sets to true
         if ((GameManager.instance.player.transform.position - transform.position).magnitude <= range)
         {
-            anim.SetBool("In Attack Range", true);
+            int animNumber = Random.Range(1, 4);
+            Debug.Log(animNumber);
+            anim.SetInteger("In Attack Range", animNumber);
         }
         else
         {
-            anim.SetBool("In Attack Range", false);
+            anim.SetInteger("In Attack Range", 0);
         }
     }
 
@@ -120,11 +161,53 @@ public class AIMino : MonoBehaviour, IDamage
     {
         //Turns collider on for weapon
         weapon.GetComponent<Collider>().enabled = true;
+        currSpeed = agent.speed;
+        agent.speed = 0;
+    }
+
+    void BossStageChange(bossStages bStage)
+    {
+        switch (bStage)
+        {
+            case bossStages.SECOND_STAGE:
+                StartStageTwo();
+                break;
+            case bossStages.THIRD_STAGE:
+                StartStageThree();
+                break;
+        }
     }
 
     void AttackStop()
     {
         //Turns collider off for weapon
         weapon.GetComponent<Collider>().enabled = false;
+        agent.speed = currSpeed;
+    }
+
+    void StartStageTwo()
+    {
+        agent.speed *= 1.5f;
+        stageTwoSpeed = agent.speed;
+        isGrowing = true;
+        scalar = 0;
+        anim.SetTrigger("Roar Trigger");
+        agent.stoppingDistance = 6;
+        range *= 2;
+    }
+    void StartStageThree()
+    {
+
+    }
+
+    void ChargeAttack()
+    {
+
+    }
+    float ParametricGrowthCurve(float t)
+    {
+        t /= 2;
+        float sqt = t * t;
+        return sqt / (2.0f * (sqt - t) + 1.0f);
     }
 }

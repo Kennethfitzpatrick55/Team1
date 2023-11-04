56fafd9a588aeea62a5171ebac72d6bc5c86330c
Tyler Linton <TSLinton@student.fullsail.edu>
2023-11-04 14:10:36 -0500

    Quick Restart and final bug fix

--- Assets/Scripts/ArrowTrap.cs
+++ Assets/Scripts/ArrowTrap.cs
@@ -1,31 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class ArrowTrap : MonoBehaviour
-{
-    private GameObject arrowPrefab;
-    private Transform player;
-    float arrowSpeed = 2;
+//public class ArrowTrap : MonoBehaviour
+//{
+//    private GameObject arrowPrefab;
+//    private Transform player;
+//    float arrowSpeed = 2;
 
-    private bool isPressed = false;
+//    private bool isPressed = false;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player") && !isPressed)
-        {
-            isPressed = true;
-            SpawnArrow();
-        }
-    }
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if(other.CompareTag("Player") && !isPressed)
+//        {
+//            isPressed = true;
+//            SpawnArrow();
+//        }
+//    }
 
-    private void SpawnArrow()
-    {
-        Vector3 direction = (player.position - transform.position).normalized;
-        GameObject arrow = Instantiate(arrowPrefab, transform.position + Vector3.up, Quaternion.identity);
-        ArrowMovement arrowMovement = arrow.GetComponent<ArrowMovement>();
-    }
+//    private void SpawnArrow()
+//    {
+//        Vector3 direction = (player.position - transform.position).normalized;
+//        GameObject arrow = Instantiate(arrowPrefab, transform.position + Vector3.up, Quaternion.identity);
+//        ArrowMovement arrowMovement = arrow.GetComponent<ArrowMovement>();
+//    }
 
     
     
-}
+//}
